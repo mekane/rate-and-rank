09b651a71833715a61283b0c03e421cbd4002c9f
Marty Kane <marty.kane@cmegroup.com>
2020-06-09 23:14:24 -0500

    pulled out some more helper functions

--- view/src/Row.js
+++ view/src/Row.js
@@ -1,11 +1,13 @@
 import {GridCell} from './GridCell';
 import {RankCell} from "./RankCell";
 import {getGridRowStyles} from "./viewHelpers";
-import {addActiveDragClassToParentGrid, removeActiveDragClassFromParentGrid} from "./rowDragHelpers";
-const h = require('snabbdom/h').default;
+import {
+    addActiveDragClassToParentGrid,
+    allowRowDrops, dragEnded, dragLeave, getDraggingRowFromData,
+    initDataTransfer
+} from "./rowDragHelpers";
 
-//TODO: refactor out duplicated code
-const rowDataType = 'text/actionjson';
+const h = require('snabbdom/h').default;
 
 export function Row(rowValues, rowIndex, columns, actionDispatch) {
     const rankCell = RankCell(rowIndex, actionDispatch);
@@ -37,33 +39,7 @@ export function Row(rowValues, rowIndex, columns, actionDispatch) {
         addActiveDragClassToParentGrid(el);
         el.classList.add('dragging');
 
-        dt.setData(rowDataType, rowData);
-        dt.effectAllowed = 'move';
-    }
-
-    //TODO: refactor common code to helpers
-    function allowRowDrops(event) {
-        if (couldDropRowHere(event.dataTransfer)) {
-            event.preventDefault();
-            event.currentTarget.classList.add('drophighlight');
-        }
-    }
-
-    //TODO: refactor common code to helpers
-    function couldDropRowHere(dt) {
-        return (dt.effectAllowed === 'move' && dt.types.includes(rowDataType));
-    }
-
-    function dragLeave(event) {
-        //TODO: research currentTarget vs. target
-        const el = event.currentTarget;
-        el.classList.remove('drophighlight');
-    }
-
-    function dragEnded(event) {
-        console.log('row drag ended');
-        const el = event.currentTarget;
-        removeActiveDragClassFromParentGrid(el);
+        initDataTransfer(dt, rowData);
     }
 
     function rowDropped(event) {
@@ -77,22 +53,5 @@ export function Row(rowValues, rowIndex, columns, actionDispatch) {
 
             actionDispatch(moveRow);
         }
-    }
-
-    function getDraggingRowFromData(event) {
-        const dataString = event.dataTransfer.getData(rowDataType);
-
-        if (dataString === '')
-            return {}; //ignore non-row drops
-
-        let moveRow = {};
-
-        try {
-            moveRow = JSON.parse(dataString);
-        } catch (err) {
-            console.log('Error processing row drop ' + err);
-        }
-
-        return moveRow;
     }
 }
--- view/src/rowDragHelpers.js
+++ view/src/rowDragHelpers.js
@@ -1,3 +1,39 @@
+const rowDataType = 'text/actionjson';
+
+export function initDataTransfer(dt, rowData) {
+    dt.setData(rowDataType, rowData);
+    dt.effectAllowed = 'move';
+
+}
+
+export function getDraggingRowFromData(event) {
+    const dataString = event.dataTransfer.getData(rowDataType);
+
+    if (dataString === '')
+        return {}; //ignore non-row drops
+
+    //TODO: does this fire a bogus event if we return this?
+    let moveRow = {};
+
+    try {
+        moveRow = JSON.parse(dataString);
+    } catch (err) {
+        console.log('Error processing row drop ' + err);
+    }
+
+    return moveRow;
+}
+
+export function allowRowDrops(event) {
+    if (couldDropRowHere(event.dataTransfer)) {
+        event.preventDefault();
+        event.currentTarget.classList.add('drophighlight');
+    }
+}
+
+function couldDropRowHere(dt) {
+    return (dt.effectAllowed === 'move' && dt.types.includes(rowDataType));
+}
 
 export function addActiveDragClassToParentGrid(el) {
     const gridContainer = el.closest('.grid');
@@ -7,4 +43,16 @@ export function addActiveDragClassToParentGrid(el) {
 export function removeActiveDragClassFromParentGrid(el) {
     const gridContainer = el.closest('.grid');
     gridContainer.classList.remove('drag-active');
+}
+
+export function dragLeave(event) {
+    //TODO: research currentTarget vs. target
+    const el = event.currentTarget;
+    el.classList.remove('drophighlight');
+}
+
+export function dragEnded(event) {
+    console.log('row drag ended');
+    const el = event.currentTarget;
+    removeActiveDragClassFromParentGrid(el);
 }
