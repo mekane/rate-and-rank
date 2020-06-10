0b1274d5e7e04d82c389128af0f08ce5d0d1d35a
Marty Kane <marty.kane@cmegroup.com>
2020-06-09 20:54:14 -0500

    move the global undo action stuff into a separate module so it can be used in the server-side rendered page as well

--- view/index.jsonApiClient.js
+++ view/index.jsonApiClient.js
@@ -1,9 +1,16 @@
 import JsonApiActionDispatcher from './src/JsonApiActionDispatcher';
 import DataGridView from './src/DataGridView';
+import initializeGlobalGridControls from "./src/globalGridControls";
 
 export function initGrid(selector, actionUrl) {
     const el = document.querySelector(selector);
     const apiActionDispatcher = JsonApiActionDispatcher(actionUrl);
     DataGridView(el, apiActionDispatcher);
-    console.log('DataGrid initialized on element ' + selector, el);
+    console.log('DataGrid initialized on element ' + selector);
+
+    initializeGlobalGridControls();
+    /* Note for later: this now causes the undo history to disappear on page
+     * refresh since the global lists on the window are empty;
+     * Could it work to always try dispatching an undo action?
+     */
 }
---
+++ view/src/globalGridControls.js
@@ -0,0 +1,97 @@
+import waitForDocumentReady from "./documentReady";
+
+window.dataGridUndos = [];
+window.dataGridRedos = [];
+
+export default function initializeGlobalGridControls() {
+    waitForDocumentReady(document)
+        .then(initGlobalUndoHandler);
+}
+
+
+function initGlobalUndoHandler() {
+    const body = document.querySelector('body');
+    body.addEventListener('keyup', handleKeys);
+
+    const gridControls = document.createElement('div');
+    gridControls.className = 'grid-controls';
+    gridControls.addEventListener('mouseenter', updateUndoRedoButtonStates);
+
+    const undoButton = document.createElement('button');
+    undoButton.className = 'grid-controls__undo-button';
+    undoButton.textContent = 'Undo';
+    undoButton.addEventListener('click', globalUndo);
+
+    const redoButton = document.createElement('button');
+    redoButton.className = 'grid-controls__redo-button';
+    redoButton.textContent = 'Redo';
+    redoButton.addEventListener('click', globalRedo);
+
+    gridControls.appendChild(undoButton);
+    gridControls.appendChild(redoButton);
+    body.appendChild(gridControls);
+
+    updateUndoRedoButtonStates();
+}
+
+function handleKeys(e) {
+    let key = e.key;
+
+    if (e.ctrlKey)
+        key = `ctrl+${key}`;
+
+    switch (key) {
+        case "ctrl+z":
+            globalUndo();
+            break;
+        case "ctrl+Z":
+            globalRedo();
+            break;
+    }
+}
+
+function globalUndo() {
+    const lastUndoRedoCommand = window.dataGridUndos.pop();
+    console.log('Global Undo', window.dataGridUndos);
+    if (lastUndoRedoCommand) {
+        lastUndoRedoCommand.undo();
+        window.dataGridRedos.push(lastUndoRedoCommand);
+    }
+    updateUndoRedoButtonStates();
+}
+
+function globalRedo() {
+    const firstRedoCommand = window.dataGridRedos.shift();
+    console.log('Global Redo', window.dataGridRedos);
+    if (firstRedoCommand) {
+        firstRedoCommand.redo();
+        window.dataGridUndos.push(firstRedoCommand);
+    }
+    updateUndoRedoButtonStates();
+}
+
+function updateUndoRedoButtonStates() {
+    const undoButton = document.querySelector('.grid-controls__undo-button');
+    const redoButton = document.querySelector('.grid-controls__redo-button');
+
+    if (undoButton == null || redoButton == null)
+        return;
+
+    if (thereAreUndos())
+        undoButton.classList.remove('grid-controls__undo-button--disabled');
+    else
+        undoButton.classList.add('grid-controls__undo-button--disabled');
+
+    if (thereAreRedos())
+        redoButton.classList.remove('grid-controls__redo-button--disabled');
+    else
+        redoButton.classList.add('grid-controls__redo-button--disabled');
+}
+
+function thereAreUndos() {
+    return window.dataGridUndos && window.dataGridUndos.length;
+}
+
+function thereAreRedos() {
+    return window.dataGridRedos && window.dataGridRedos.length;
+}
